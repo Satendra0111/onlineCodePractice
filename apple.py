f144a0c8ba6e9ca585e7385da20435b43c21296c
Sam12993a <gragerssam@gmail.com>
2023-09-01 22:31:23 +0530

    new Apple in a house Code

---
+++ apple.py
@@ -0,0 +1,68 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countApplesAndOranges' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+#
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    
+    appleLand=[]
+    orangeLand=[]
+    counta=0
+    counto=0
+    for i in apples:
+        appleLand.append(i+a)
+    for j in oranges:
+        orangeLand.append(j+b)
+    
+    for i in appleLand:
+        if s <= i <= t:
+            counta+=1
+            
+    for i in orangeLand:
+        if s <= i <= t:
+            counto+=1  
+            
+    print (counta)
+    print (counto)
+    # Write your code here
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)
+
